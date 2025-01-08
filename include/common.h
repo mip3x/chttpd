6b449fc13145d74c36be6e910f3845a410f080f0
mip3x <plombiiiri@yandex.ru>
2025-01-08 16:15:08 +0300

    feat: common utils

---
+++ include/common.h
@@ -0,0 +1,11 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stddef.h>
+
+#define SERVER_PORT 14880
+
+_Noreturn void  err(const char* fmt, ...);
+char* bin2hex(const unsigned char* input, size_t len);
+
+#endif
