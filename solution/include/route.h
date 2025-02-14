2bbf3716f322da4b95a04efe13e083e4a1ec0630
mip3x <plombiiiri@yandex.ru>
2025-02-15 00:06:17 +0300

    chore: macro PRINT_ROUTE & changes

--- solution/include/route.h
+++ solution/include/route.h
@@ -1,6 +1,8 @@
 #ifndef ROUTE_H
 #define ROUTE_H
 
+#include "common.h"
+
 typedef struct {
 #define CONFIG_WEB_ROOT "__CONFIG_WEB_ROOT__"
     char* web_root;
@@ -10,5 +12,12 @@ typedef struct {
 } route;
 
 void print_route(route route);
+
+#define PRINT_ROUTE(route) \
+    debug(__func__, "route:\n\tweb_root: %s\n\tmapping: %s\n\tfile_path: %s", \
+        (route).web_root, \
+        (route).mapping, \
+        (route).file_path \
+    );
 
 #endif
