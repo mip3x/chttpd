6ab4b13e065d1eafabd596af2f8286f32e56e300
mip3x <plombiiiri@yandex.ru>
2025-02-15 13:45:00 +0300

    wip(demo): install extracted link finished

--- solution/include/extractor.h
+++ solution/include/extractor.h
@@ -1,7 +1,6 @@
 #ifndef EXTRACTOR_H
 #define EXTRACTOR_H
 
-#define REFERENCED_FILES 10
 #define BUFFER_SIZE 512
 
 void explore_routes();
--- solution/src/extractor.c
+++ solution/src/extractor.c
@@ -15,25 +15,11 @@ const char* patterns[] = {
     "<script[^>]+src=\"([^\"]+)\""
 };
 
-static void free_extracted_links(char** const extracted_links, size_t count) {
-    if (!extracted_links) return;
-
-    for (size_t i = 0; i < count; i++)
-        if (extracted_links[i]) free(extracted_links[i]);
-    free(extracted_links);
-}
-
-static void extract_links(route* route) {
-    char** extracted_links = malloc(REFERENCED_FILES * sizeof(char*));
-    if (!extracted_links) {
-        err("failed to allocate memory for extracted_links\n");
-        return;
-    }
-
-    PRINT_ROUTE(*route);
+static void extract_links(route* incoming_route) {
+    PRINT_ROUTE(*incoming_route);
 
     char* file_path_with_web_root = NULL;
-    asprintf(&file_path_with_web_root, "%s%s", route->web_root, route->file_path);
+    asprintf(&file_path_with_web_root, "%s%s", incoming_route->web_root, incoming_route->file_path);
 
     size_t file_size;
     char* file_content = read_file(file_path_with_web_root, &file_size);
@@ -48,8 +34,6 @@ static void extract_links(route* route) {
     const size_t nmatch = 2;
     regmatch_t pmatch[nmatch + 1];
 
-    size_t extracted_links_count = 0;
-
     for (size_t i = 0; i < patterns_length; i++) {
         if (regcomp(&preg, patterns[i], REG_EXTENDED) != 0) {
             err("failed to compile pattern\n");
@@ -63,22 +47,29 @@ static void extract_links(route* route) {
             size_t extracted_link_length = extracted_link_regmatch_t.rm_eo - extracted_link_regmatch_t.rm_so;
             char* extracted_link = strndup(search_start + extracted_link_regmatch_t.rm_so, extracted_link_length);
 
-            extracted_links[extracted_links_count++] = extracted_link;
             debug(__func__, "extracted link: %s", extracted_link);
 
+            char buf[BUFFER_SIZE];
+            snprintf(buf, sizeof buf, "/%s", extracted_link);
+
+            route extracted_link_route = {
+                .web_root = strdup(incoming_route->web_root),
+                .file_path = strdup(extracted_link),
+                .mapping = strdup(buf),
+            }; 
+
+            install(extracted_link_route);
+
+            debug(__func__, "web_root + extracted_link path: %s", extracted_link_route.mapping);
+    
             search_start += extracted_link_regmatch_t.rm_eo;
+            free(extracted_link);
         }
     }
 
     regfree(&preg);
     free(file_content);
-
-    debug(__func__, "file_path_with_web_root: %s", file_path_with_web_root);
-
-    // TODO: install routes
-    
     free(file_path_with_web_root);
-    free_extracted_links(extracted_links, extracted_links_count);
 }
 
 /*
