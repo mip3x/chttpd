5dd40436583d3af8a421968344960c03390907d9
mip3x <plombiiiri@yandex.ru>
2025-03-01 02:10:53 +0300

    wip: added new error statuses

--- solution/include/status.h
+++ solution/include/status.h
@@ -3,7 +3,15 @@
 
 typedef enum {
     OK,
-    ERROR
+    OPEN_FILE_ERROR,
+    MALLOC_FILE_CONTENT_ERROR,
+    READ_FILE_ERROR,
+    SERVER_POINTER_ERROR,
+    SOCKET_ERROR,
+    SETSOCKOPT_ERROR,
+    BIND_ERROR,
+    LISTEN_ERROR,
+    ACCEPT_ERROR
 } status;
 
 #endif
